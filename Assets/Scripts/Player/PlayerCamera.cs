c89effa0d5bf841de9b8b0d7fbf016b2dc5e8249
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-03-09 00:34:20 +0800

    feat: изменено большое здание
    Переработана структура и внешний вид большого здания. Добавлены новые элементы и улучшено окружение.

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -2,20 +2,13 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    private Transform _player;
     [SerializeField] private float _smoothSpeed = 5f;
     [SerializeField] private Vector3 _offset = new Vector3(0f, 10f, -10f);
-    [SerializeField] private float _minDistance = 0.2f;
-    [SerializeField] private LayerMask _collisionLayer;
-    [SerializeField] private float _maxCameraAngle = 60f;
-
-    private Transform _player;
-    private CameraCollisionHandler _collisionHandler;
 
     private void Start()
     {
         FindPlayer();
-        // ѕередаем слой столкновений в конструктор
-        _collisionHandler = new CameraCollisionHandler(_minDistance, _collisionLayer);
     }
 
     private void FixedUpdate()
@@ -32,31 +25,7 @@ public class PlayerCamera : MonoBehaviour
     private void FollowPlayer()
     {
         Vector3 targetPosition = _player.position + _offset;
-
-        // ѕрилепл€ем камеру к преп€тстви€м по оси Z
-        if (_collisionHandler.TryGetAdjustedPosition(_player.position, targetPosition, out targetPosition))
-        {
-            // ќграничиваем угол наклона камеры по оси X
-            ClampCameraAngle(ref targetPosition);
-        }
-
-        // ”станавливаем позицию камеры
-        transform.position = targetPosition;
-        transform.LookAt(_player); //  амера всегда смотрит на игрока
-    }
-
-    private void ClampCameraAngle(ref Vector3 targetPosition)
-    {
-        // –ассчитываем угол наклона камеры по оси X
-        Vector3 direction = targetPosition - _player.position;
-        float angle = Vector3.Angle(Vector3.up, direction);
-
-        // ≈сли угол наклона больше максимально допустимого, ограничиваем его
-        if (angle > _maxCameraAngle)
-        {
-            direction = Quaternion.Euler(_maxCameraAngle, 0f, 0f) * direction;
-            targetPosition = _player.position + direction;
-        }
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothSpeed * Time.deltaTime);
     }
 
     private void FindPlayer()
